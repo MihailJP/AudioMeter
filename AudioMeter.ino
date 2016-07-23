de172d705ac286020c7f3ffcd3794421982daab5
MihailJP <mihailjp@gmail.com>
2016-07-23 23:34:39 +0900

    Initialize LCD interface

--- AudioMeter.ino
+++ AudioMeter.ino
@@ -1,6 +1,15 @@
-void setup() {
-  // put your setup code here, to run once:
+#include <S1d13781_gfx.h>
+#include <S1d13781_registers.h>
 
+/* THIS SKETCH RUNS ON ARDUINO DUE ONLY!! */
+
+// LCD interface S5U13781R01C100
+// Logic voltage == 3.3V (Arduino Due only)
+S1d13781_gfx lcd;
+
+void setup() {
+  /* Initialize LCD */
+  lcd.begin();
 }
 
 void loop() {
