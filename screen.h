506e60d7bcc8da7efd21cbaea1ee7868aec01f37
MihailJP <mihailjp@gmail.com>
2016-07-24 18:34:30 +0900

    Plotting window

--- screen.h
+++ screen.h
@@ -6,12 +6,17 @@
 // LCD interface S5U13781R01C100
 // Logic voltage == 3.3V (Arduino Due only)
 
+#define BACKGROUND_COLOR 0x00808080
+#define HIGHLIGHT_COLOR  0x00c0c0c0
+#define SHADOW_COLOR     0x00404040
+
 // Position and size of the Lissajous view
-#define LISSAJOUS_X 0
-#define LISSAJOUS_Y 0
+#define LISSAJOUS_X 8
+#define LISSAJOUS_Y 8
 #define LISSAJOUS_SIZE 128 /* must be square */
 #define LISSAJOUS_TRACE_COLOR 0x0004110c
 #define LISSAJOUS_TRACE_INTENSITY_FACTOR 16
+#define LISSAJOUS_WINDOW_PADDING 4
 
 class Screen;
 
@@ -35,5 +40,9 @@ public:
   unsigned short getWidth();
   unsigned short getHeight();
   void pset(int, int, int);
+  void line(int, int, int, int, int);
+  void bevel(int, int, int, int);
+  void boxf(int, int, int, int, int);
+  void bevelBoxf(int, int, int, int, int);
   void clear();
 };
--- screen.ino
+++ screen.ino
@@ -10,7 +10,7 @@ void Screen::init() {
   lcd.lcdSetColorDepth(S1d13781::format_RGB_888);
   width = lcd.lcdGetWidth();
   height = lcd.lcdGetHeight();
-  lcd.clearWindow(S1d13781_gfx::window_Main);
+  lcd.fillWindow(S1d13781_gfx::window_Main, BACKGROUND_COLOR);
 
   logger.print(F("Screen Address: ")); logger.print((int)this, HEX);
   logger.print(F(", Width: ")); logger.print(width);
@@ -33,12 +33,39 @@ void Screen::pset(int x, int y, int color) {
   isFirstTime = false;
 }
 
+void Screen::line(int x1, int y1, int x2, int y2, int color) {
+  lcd.drawLine(S1d13781_gfx::window_Main, x1, y1, x2, y2, color);
+}
+
+void Screen::bevel(int x, int y, int w, int h) {
+  line(x, y, x + w, y, SHADOW_COLOR);
+  line(x, y, x, y + h, SHADOW_COLOR);
+  line(x + w, y, x + w, y + h, HIGHLIGHT_COLOR);
+  line(x, y + h, x + w, y + h, HIGHLIGHT_COLOR);
+}
+
+void Screen::boxf(int x, int y, int w, int h, int color) {
+  lcd.drawFilledRect(S1d13781_gfx::window_Main, x, y, w, h, color);
+}
+
+void Screen::bevelBoxf(int x, int y, int w, int h, int color) {
+  bevel(x - 1, y - 1, w + 1, h + 1);
+  boxf(x, y, w, h, color);
+}
+
 void Screen::clear() {
   lcd.clearWindow(S1d13781_gfx::window_Main);
 }
 
 void LissajousView::init(Screen* scr) {
   screen = scr;
+
+  screen->bevelBoxf(
+    LISSAJOUS_X - LISSAJOUS_WINDOW_PADDING,
+    LISSAJOUS_Y - LISSAJOUS_WINDOW_PADDING,
+    LISSAJOUS_SIZE + LISSAJOUS_WINDOW_PADDING * 2,
+    LISSAJOUS_SIZE + LISSAJOUS_WINDOW_PADDING * 2,
+    0x00000000);
   logger.print(F("Lissajous window X: ")); logger.print(LISSAJOUS_X);
   logger.print(F(", Y: ")); logger.print(LISSAJOUS_Y);
   logger.print(F(", Size: ")); logger.println(LISSAJOUS_SIZE);
