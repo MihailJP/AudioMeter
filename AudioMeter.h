28c373ec9065625e01d1a6fa696d1c54959d7ba7
MihailJP <mihailjp@gmail.com>
2016-07-24 11:47:56 +0900

    Add AudioMeter.h for globals

---
+++ AudioMeter.h
@@ -0,0 +1,6 @@
+#pragma once
+
+extern Screen screen; // LCD Screen
+extern DebugLogger logger; // Debug logger
+extern Sampler sampler; // Sampler
+extern Timer timer; // Timer (external clock interrupt)
