2d3c979410810d15a1fbf25c75681dad97c0f34e
MihailJP <mihailjp@gmail.com>
2016-07-30 22:26:21 +0900

    Magnify Lissajous plot window

--- lissajous.h
+++ lissajous.h
@@ -3,7 +3,7 @@
 // Position and size of the Lissajous view
 #define LISSAJOUS_X 8
 #define LISSAJOUS_Y 8
-#define LISSAJOUS_SIZE 128 /* must be square */
+#define LISSAJOUS_SIZE 256 /* must be square */
 #define LISSAJOUS_TRACE_COLOR 0x0004110c
 #define LISSAJOUS_TRACE_INTENSITY_FACTOR 15
 #define LISSAJOUS_WINDOW_PADDING 4
