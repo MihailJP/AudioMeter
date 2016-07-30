4062b54d7515f68b8598e560d8777a05e5a0f7e9
MihailJP <mihailjp@gmail.com>
2016-07-30 22:24:35 +0900

    Compress lissajous buffer
    4 bits per point

--- lissajous.h
+++ lissajous.h
@@ -5,7 +5,7 @@
 #define LISSAJOUS_Y 8
 #define LISSAJOUS_SIZE 128 /* must be square */
 #define LISSAJOUS_TRACE_COLOR 0x0004110c
-#define LISSAJOUS_TRACE_INTENSITY_FACTOR 16
+#define LISSAJOUS_TRACE_INTENSITY_FACTOR 15
 #define LISSAJOUS_WINDOW_PADDING 4
 #define LISSAJOUS_AXIS_COLOR 0x003c1d00
 #define LISSAJOUS_TRACE_COLOR_ON_AXIS 0x000d0d0d
@@ -18,7 +18,7 @@ private:
   Screen* screen;
   short enq_prescaler;
   short deq_prescaler;
-  byte buffer[LISSAJOUS_SIZE][LISSAJOUS_SIZE];
+  byte buffer[LISSAJOUS_SIZE / 2][LISSAJOUS_SIZE];
   void plot(short, short, short);
 public:
   void init(Screen*);
